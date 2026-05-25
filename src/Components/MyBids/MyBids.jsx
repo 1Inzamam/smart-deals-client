046d403c889151e0cd36312ca332b413e5bdd204
Md Inzamam Ul Khan <inzamamkhan618@gmail.com>
2026-05-25 11:05:36 +0600

    sending accessToken from client side to server side by headers:{authorization: }

--- src/Components/MyBids/MyBids.jsx
+++ src/Components/MyBids/MyBids.jsx
@@ -7,7 +7,11 @@ const MyBids = () => {
   const [bids, setBids] = useState([]);
   useEffect(() => {
     if (user?.email) {
-      fetch(`http://localhost:3000/bids?email=${user.email}`)
+      fetch(`http://localhost:3000/bids?email=${user.email}`, {
+        headers: {
+          authorization: `Bearer ${user.accessToken}`
+        }
+      })
         .then((res) => res.json())
         .then((data) => {
           console.log(data);
